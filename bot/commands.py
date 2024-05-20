ec75d068a4d7ad9775dc632a344f26320099d29e
jiang <lvyang2010@sina.com>
2024-05-20 21:02:19 +0800

    revise

--- bot/commands.py
+++ bot/commands.py
@@ -5,7 +5,7 @@ async def start(update: Update, context):
     await update.message.reply_text('你好，感谢你与我聊天，我是 Lisa 机器人。')
 
 async def help(update: Update, context):
-    await update.message.reply_text('我是 Lisa 机器人，请随意输入信息，我会进行回复。')
+    await update.message.reply_text('我是 Lisa 机器人，输入[城市]天气，可以告诉该城市明天的天气情况。')
 
 async def custom(update: Update, context):
     await update.message.reply_text('这是自定义命令')
